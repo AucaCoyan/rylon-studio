5ad26bf22b25843f6dd531c439600f6458f38078
Auca Maillot <auca.maillot@farmacity.com.ar>
2023-04-10 12:19:02 -0300

    :sparkles: add logo and first section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
+import Image from "next/image";
+import LogoRylonStudio from "../../public/Logo rylonstudio con texto.png";
+import Separador01 from "../../public/Separador01.png";
 
 export default function Home() {
 	return (
@@ -10,10 +12,27 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.png" />
 			</Head>
-			<main className="flex h-screen bg-primary-orange">
-				<div className="m-auto text-6xl text-center text-secondary-violet">
-					Sitio en contrucción
-				</div>
+			<main className="">
+				<section className="flex  h-screen flex-col">
+					<div className="m-auto">
+						<Image
+							src={LogoRylonStudio}
+							alt="Logo Rylon Studio"
+							width={412}
+							height={140}
+						/>
+					</div>
+					<div className="m-auto p-10 text-6xl text-center text-secondary-violet">
+						Sitio en contrucción
+					</div>
+					<div className="mx-auto pb-0">
+						<Image
+							src={Separador01}
+							alt="Separador de la primera sección"
+							width={1500}
+						/>
+					</div>
+				</section>
 			</main>
 		</>
 	);
